bbdcc0574704a7f269f5246ac55e14b5b48db9e6
egor27032005 <sergeevegor2005@gmail.com>
2022-10-04 12:05:16 -0400

    сделал дз

---
+++ Task64/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите число ");
+int n = Convert.ToInt32(Console.ReadLine());
+Sequence(1);
+void Sequence(int i)
+{
+    if (i==n+1) return;
+    Sequence(i+1);
+    Console.Write(i+" ");
+    
+}
---
+++ Task66/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите первое число ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число ");
+int n = Convert.ToInt32(Console.ReadLine());
+Sequence(1);
+int Sequence(int n)
+{
+    if (n==m) return m;
+    else return n+Sequence(n-1);
+    
+    
+}
+Console.Write("сумма элементов от n до m: " +Sequence(n));
